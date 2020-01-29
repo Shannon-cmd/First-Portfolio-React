154f661d3797ace7d54a614276ee2fd87c35b5a5
Shannon <stoshannon@gmail.com>
2020-01-29 15:19:26 +0100

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import Loading from './components/Loading';
 import Header from './components/Header';
 import About from './components/About';
 import Skills from './components/Skills';
@@ -8,18 +9,33 @@ import Form from './components/Form';
 import Footer from './components/Footer';
 
 const App = () => {
-  return (
-    <Fragment>
-      <Header />
-      <About />
-      <Skills />
-      <Service />
-      <Portfolio />
-      <Form />
-      <Footer />
-    </Fragment>
-  )
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true)
+    }, 1700);
+  })
+  if (loading === true) {
+    return (
+      <Fragment>
+        <Header />
+        <About />
+        <Skills />
+        <Service />
+        <Portfolio />
+        <Form />
+        <Footer />
+      </Fragment>
+    )
+  } else {
+    return (
+      <Fragment>
+        <Loading />
+      </Fragment>
+    )
+  }
+}
 
-};
+
 
 export default App;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import '../scss/_Loading.scss';
+
+const Loading = () => {
+  return(
+    <Fragment>
+      <div>
+        <div class="loading">Loading&#8230;</div>
+      </div>
+    </Fragment>
+  )
+}
+
+
+export default Loading;
